16ff889619dce1118944a44a63564e4fed1d9aea
Tmoil <140699615+Tmoil1@users.noreply.github.com>
2024-02-10 21:10:08 +0200

    Update indexs.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Eating Watermelon"];
 
 
 let currentIndex = 0;
